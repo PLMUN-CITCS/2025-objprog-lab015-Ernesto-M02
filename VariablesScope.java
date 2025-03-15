3f108b30852aa1c23dc8fa55668694aa0ad80783
Ernesto Marpuri <marpuriernesto_bscs@plmun.edu.ph>
2025-03-14 23:36:51 -0700

    Submitting OBJPROG Week 05 - Laboratory # 15

--- VariablesScope.java
+++ VariablesScope.java
@@ -1,22 +1,15 @@
 public class VariablesScope {
-    // Global variable
-    static int globalCount = 100;
-
-    public static void main(String[] args) {
-        // Print the global count from the main method
-        System.out.println("Global Count (from main): " + globalCount);
-        
-        // Call the showScope method
-        showScope();
+     static int globalCount = 100; 
+     //... (globalCount variable)...
+      public static void main(String[] args) {
+         System.out.println("Global Count (from main): " + globalCount); 
+         
+         //...(other code in main)...
+          showScope(); 
+          } 
+          public static void showScope() {
+             //...(main method)... 
+             int localCount = 50; 
+             System.out.println("Global Count (from showScope): " + globalCount); 
+             System.out.println("Local Count (from showScope): " + localCount); } 
     }
-
-    public static void showScope() {
-        // Local variable
-        int localCount = 50;
-        
-        // Print the global and local counts
-        System.out.println("Global Count (from showScope): " + globalCount);
-        System.out.println("Local Count (from showScope): " + localCount); 
-    }
-}
-
